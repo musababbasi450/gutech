bdca0643ef56166a2e3acd89eade89513aa9d45d
musababbasi450 <musab>
2024-11-28 21:09:05 +0500

    second

---
+++ lab07/src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css'; // Import global styles
+import Navbar from './component/navbar';
+import LandingPage from './component/landingpage';
+
+function App() {
+return (
+<div>
+<Navbar />
+<LandingPage />
+</div>
+
+);
+}
+
+export default App;
--- musab/src/components/footer.jsx
+++
@@ -1,4 +0,0 @@
-function Content() {
-    return <h1 class="footer">© 2024 My React App</h1>;
-    }
-    export default Content;
